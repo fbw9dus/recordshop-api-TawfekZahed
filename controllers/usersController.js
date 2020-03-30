80d40b46cf85286a24487104825be7e5f6deb98f
https://github.com/AlMuthana1992/m.hasan.git <test@test.test>
2020-03-30 11:24:56 +0200

    test1

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,22 +1,28 @@
 const {  validationResult } = require('express-validator');
-const User = require('../models/User');
+const Users = require('../models/User');
+
+
 exports.getUsers = async (req, res, next) => {
   // Schreib hier code um alle Kunden aus der users-Collection zu holen
   var users = await Users.find()
+
   res.status(200).send(users);
 };
+
 exports.getUser = async (req, res, next) => {
   const { id } = req.params;
   // Schreib hier code um den Kunden mit der id aus params aus der users-Collection zu holen
   var user = await Users.findById(id)
   res.status(200).send(user);
 };
+
 exports.deleteUser = async (req, res, next) => {
   const { id } = req.params;
   // Schreib hier code um den Kunden mit der id aus params aus der users-Collection zu löschen
   var user = await Users.findByIdAndDelete(id)
   res.status(200).send(user);
 };
+
 exports.updateUser = async (req, res, next) => {
   const { id } = req.params;
   const dt = req.body;
@@ -25,7 +31,10 @@ exports.updateUser = async (req, res, next) => {
   var user = await Users.findByIdAndUpdate(id, dt, {new: true})
   res.status(200).send(user);
 };
+
+
 exports.addUser =  async (req, res, next) => {
+
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     return res.status(422).json({ errors: errors.array() })
