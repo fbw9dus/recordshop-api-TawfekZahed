2fc5d5226d9e2793bc6c4670fa5ae00f90be2b3f
https://github.com/AlMuthana1992/m.hasan.git <test@test.test>
2020-03-30 11:11:41 +0200

    test

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,38 +1,38 @@
-var Users = require('../models/User');
-
+const {  validationResult } = require('express-validator');
+const User = require('../models/User');
 exports.getUsers = async (req, res, next) => {
   // Schreib hier code um alle Kunden aus der users-Collection zu holen
   var users = await Users.find()
-
   res.status(200).send(users);
 };
-
 exports.getUser = async (req, res, next) => {
   const { id } = req.params;
   // Schreib hier code um den Kunden mit der id aus params aus der users-Collection zu holen
   var user = await Users.findById(id)
   res.status(200).send(user);
 };
-
 exports.deleteUser = async (req, res, next) => {
   const { id } = req.params;
   // Schreib hier code um den Kunden mit der id aus params aus der users-Collection zu löschen
   var user = await Users.findByIdAndDelete(id)
   res.status(200).send(user);
 };
-
 exports.updateUser = async (req, res, next) => {
   const { id } = req.params;
   const dt = req.body;
   // Schreib hier code um den User mit der id aus params in der users-Collection mit den Daten aus req.body zu aktualisieren
+  
   var user = await Users.findByIdAndUpdate(id, dt, {new: true})
   res.status(200).send(user);
 };
-
-exports.addUser = async (req, res, next) => {
-  const data = req.body;
+exports.addUser =  async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({ errors: errors.array() })
+  }
+  const user = req.body;
   // Schreib hier code um die Daten des neuen Kunden aus req.body in der users-Collection zu speichern
-  var user = new Users(data)
-  await user.save()
+  var newUser = new Users(user)
+  await newUser.save()
   res.status(200).send(user);
 };
